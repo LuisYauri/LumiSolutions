650af4b2be8d65e43b1cfffeb6f920aad4fc16a7
Luis Yauri <david.yauri147@gmail.com>
2019-10-29 05:30:41 -0500

    Conexion rutas

--- DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/app-routing.module.ts
+++ DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./layouts/Login/login.component";
+import {StudentComponent} from "./layouts/Student/student.component";
+import {LayoutsModule} from "./layouts/layouts.module";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent,
+    children: [
+      {
+        path: 'homework',
+        loadChildren: 'src/app/modules/Student/Homework/homework-student.module#HomeworkStudentModule',
+      },
+      {
+        path: 'history',
+        loadChildren: 'src/app/modules/Student/History/history-student.module#HistoryStudentModule',
+      },
+      {
+        path: 'exam',
+        loadChildren: 'src/app/modules/Student/Exam/exam-student.module#ExamStudentModule',
+      },
+      {
+        path: 'criteria',
+        loadChildren: 'src/app/modules/Student/Criteria/criteria-student.module#CriteriaStudentModule',
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),LayoutsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/layouts/layouts.module.ts
+++ DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/layouts/layouts.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
+import {SharedModule} from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './Login/login.component';
 import { StudentComponent } from './Student/student.component';
 import { TeacherComponent } from './Teacher/teacher.component';
 
-
+const COMPONENTS = [LoginComponent, StudentComponent, TeacherComponent];
 
 @NgModule({
-  declarations: [LoginComponent, StudentComponent, TeacherComponent],
+  declarations: [...COMPONENTS],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule
+  ],
+  exports:[...COMPONENTS]
 })
 export class LayoutsModule { }
