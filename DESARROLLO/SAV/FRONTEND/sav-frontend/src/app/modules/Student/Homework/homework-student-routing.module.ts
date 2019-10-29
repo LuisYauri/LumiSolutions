61423cb9e64af7ae3f67bda3fd69ecb250ec37de
Luis Yauri <david.yauri147@gmail.com>
2019-10-29 07:27:54 -0500

    actualización de stylos

--- DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/modules/Student/Homework/homework-student-routing.module.ts
+++ DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/modules/Student/Homework/homework-student-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeworkStudentComponent} from "./pages/Homework/homework-student.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeworkStudentComponent,
+    data: {title:'HomeWork'}
+  }
 ];
 
 @NgModule({
--- DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/modules/Student/Homework/homework-student.module.ts
+++ DESARROLLO/SAV/FRONTEND/sav-frontend/src/app/modules/Student/Homework/homework-student.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import {SharedModule} from '../../../shared/shared.module';
 import {HomeworkStudentRoutingModule} from "./homework-student-routing.module";
+import {HomeworkStudentComponent} from './pages/Homework/homework-student.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeworkStudentComponent],
   imports: [
     SharedModule,
     CommonModule,
     HomeworkStudentRoutingModule
   ]
 })
-export class HomeworkStudentModule { }
+export class HomeworkStudentModule {
+}
