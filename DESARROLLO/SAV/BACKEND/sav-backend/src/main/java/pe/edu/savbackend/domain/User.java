08303b856140c229dee2a1ac8aa1f450195ec300
Miguel <donadoni.tk@gmail.com>
2019-10-31 08:37:53 -0500

    estructura final y agregadas dependencias de seguridad

---
+++ DESARROLLO/SAV/BACKEND/sav-backend/src/main/java/pe/edu/savbackend/domain/User.java
@@ -0,0 +1,24 @@
+package pe.edu.savbackend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+	private String user;
+	private String password;
+	private String token;
+	
+}
