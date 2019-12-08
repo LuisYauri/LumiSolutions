e34b8a85a36641349568b590c4aae9077cb670f1
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-12-08 10:57:09 -0500

    api actualizar y eliminar aula

--- DESARROLLO/SAV/BACKEND/sav-backend/src/main/java/pe/edu/savbackend/controller/AulaController.java
+++ DESARROLLO/SAV/BACKEND/sav-backend/src/main/java/pe/edu/savbackend/controller/AulaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,4 +42,8 @@ public class AulaController {
 		return aulaService.actualizarAula(aula);
 	}
 
+	@PatchMapping(value = "/{idAula}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Boolean eliminarAula(@PathVariable Integer idAula) {
+		return aulaService.eliminarAula(idAula);
+	}
 }
